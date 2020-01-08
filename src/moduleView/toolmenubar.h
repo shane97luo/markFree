0049f512ed56f5a1287a237806ee05c5066a38f1
ShaneLuo <shaneluo97@foxmail.com>
2020-01-08 18:37:18 +0800

    Create toolmenubar.h

---
+++ src/moduleView/toolmenubar.h
@@ -0,0 +1,25 @@
+#ifndef TOOLMENUBAR_H
+#define TOOLMENUBAR_H
+
+#include <QWidget>
+#include <QPushButton>
+#include <QVBoxLayout>
+
+class ToolMenuBar : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit ToolMenuBar(QWidget *parent = nullptr);
+
+    void initMenu();
+private:
+    QPushButton *editMenu;
+    QPushButton *paintMenu;
+    QPushButton *helpMenu;
+
+signals:
+
+public slots:
+};
+
+#endif // TOOLMENUBAR_H

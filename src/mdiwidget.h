1bf13a2cd59f28401c55cda888487738099f139f
ShaneLuo <shaneluo97@foxmail.com>
2020-01-08 18:37:47 +0800

    Create mdiwidget.h

---
+++ src/mdiwidget.h
@@ -0,0 +1,17 @@
+#ifndef MDIWIDGET_H
+#define MDIWIDGET_H
+
+#include <QWidget>
+
+class MDIWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit MDIWidget(QWidget *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // MDIWIDGET_H
